fb3cac097e4b65c37c5bf96754de6cad9f0e1970
王治国 <aitwzg@163.com>
2022-04-18 17:19:54 +0800

    first commit

---
+++ src/根模块的使用用法.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    App
+    <p>{{ $store.state.username }}</p>
+    <p>{{ $store.getters['newName'] }}</p>
+    <button @click="mutationFn">改变username</button>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+export default {
+  name: 'App',
+  setup() {
+    const store = useStore()
+    console.log(store.state.username)
+    console.log(store.getters.newName)
+    const mutationFn = () => {
+      // store.commit('updateName')
+      store.dispatch('updateName')
+    }
+    return { mutationFn }
+  },
+}
+</script>
+
+<style>
+</style>
